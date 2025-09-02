e1aec2d69e85d1aa3c66b90cf2089a4d3c855d3c
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-09-02 13:27:22 +0300

    add localizations done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/configs/router/app_router.dart';
 import 'package:fruit_hub/features/splash/presentation/cubit/splash_cubit/splash_cubit.dart';
+import 'package:fruit_hub/generated/l10n.dart';
 
 void main() {
   runApp(const FruitHubApp());
@@ -17,6 +19,14 @@ class FruitHubApp extends StatelessWidget {
       child: MaterialApp.router(
         routerConfig: AppRouter.router,
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale('en'), Locale('ar')],
+        locale: Locale('ar'),
       ),
     );
   }
